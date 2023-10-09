52082a103b949e0f90a183c53a9e0630c8dba715
LathithaKunana <lathitha.kunana@younglings.africa>
2023-10-09 16:53:25 +0200

    Finished searcbar component and started with the Catergories component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,3 +1,4 @@
+import { AntDesign } from '@expo/vector-icons'
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
@@ -27,6 +28,22 @@ export default function SearchBar() {
                renderLeftButton={() => (
                 <View style={{marginLeft: 10}} >
                     <Ionicons name="location-sharp" size={24} />
+                </View>
+               )}
+               renderRightButton={() => (
+                <View 
+                    style={
+                        {
+                           flexDirection: "row",
+                           marginRight: 8,
+                           backgroundColor: "white",
+                           padding: 9,
+                           borderRadius: 30,
+                           alignItems: "center",
+                        }
+                    }>
+                        <AntDesign name='clockcircle' size={11} style={{marginRight: 6}} />
+                    <Text>Search</Text>
                 </View>
                )}
                 
