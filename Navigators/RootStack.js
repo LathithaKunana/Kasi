fce5e2feabb0cb3caec510927eed576a0aec0a12
Lathitha Kunana <130542978+LathithaKunana@users.noreply.github.com>
2023-10-11 23:35:03 +0200

    Update RootStack.js

--- Navigators/RootStack.js
+++ Navigators/RootStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContaine } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login';
 import Signup from '../screens/Signup';
